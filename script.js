3554b969738265f479d6defba8de55568d2e22fa
LauraKirby <laurakirby26@gmail.com>
2015-08-18 22:55:32 -0700

    add projects and links

--- script.js
+++ script.js
@@ -14,7 +14,49 @@ $(document).ready(function() {
       // itemsDesktopSmall : false,
       // itemsTablet: false,
       // itemsMobile : false
- 
   });
+
+
+
+
+  // //user AJAX to make this call
+  // //blog xml
+  // //var tyResult = Typhoeus.get('http://codetoempower.tumblr.com/rss');
+
+  // //parse xml, so we can interact with it with jquery
+  // var myXmlDoc = $.parseXML( tyResult );
+
+  // //wrap string in a jquery object
+  // var $myXml = $(myXmlDoc);
+
+  // //can now use jquery to search xml doc or $xml
+  // var $myTitle = $myXml.find( "title" );
+
+  // // Append "RSS Title" to #someElement
+  // $( ".mySomeElement" ).append( $myTitle.text() );
+
+
+
+  // //sample xml
+  // var xml = "<rss version='2.0'><channel><title>Adventures of Learning to Code</title></channel></rss>";
+  // console.log(xml);
+  // //parse xml, so we can interact with it with jquery
+  // var xmlDoc = $.parseXML( xml );
+  
+  // //wrap string in a jquery object
+  // var $xml = $( xmlDoc );
+
+  // //can now use jquery to search xml doc or $xml
+  // var $title = $xml.find( "title" );
+   
+  //   // Append "RSS Title" to #someElement
+  //   $( ".someElement" ).append( $title.text() );
+     
+  //   // Change the title to "XML Title"
+  //   $title.text( "XML Title" );
+     
+  //   // Append "XML Title" to #anotherElement
+  //   $( "#anotherElement" ).append( $title.text() );
+
  
-});
+}); //end doc.ready
